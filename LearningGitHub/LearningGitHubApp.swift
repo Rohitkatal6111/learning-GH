b4aad7b48e25beebf2a12a4ed5d7502fcd4d6b9a
rohit <rohitkatal@icloud.com>
2022-05-02 17:19:49 +0800

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Rohit Katal on 2/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
