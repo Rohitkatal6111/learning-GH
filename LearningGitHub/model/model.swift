1b6efea3d6951e558060b59eb4782dd172035af3
rohit <rohitkatal@icloud.com>
2022-05-02 18:16:46 +0800

    update the change

---
+++ LearningGitHub/model/model.swift
@@ -0,0 +1,18 @@
+//
+//  model.swift
+//  LearningGitHub
+//
+//  Created by Rohit Katal on 2/5/2022.
+//
+
+import Foundation
+struct model {
+    
+    
+    var robot : String = " rohit"
+    
+    
+    
+    
+    
+}
